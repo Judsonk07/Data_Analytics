f18ab7aedfc5c423509a63c5f2f87c48e7266e4c
Judsonk07 <pappamuthupappamuthu@gmail.com>
2026-09-07 17:22:56 +0530

    Learnt Variables,datatypes,operators,fstrings

---
+++ Python/Foundations/sample.py
@@ -0,0 +1,10 @@
+# print('"My Name is Arun"')
+# print(8%4,8/4,8//4)
+# print(7%4,7/4,7//4)
+print('Name: Arun \nAge: 21 \nDegree: B.Sc (Computer Science)' )
+Name="Arun"
+Age=21
+Roll_No='101'
+print("",Name,"",Age,"",Roll_No)
+name2=input("Enter Your Name: ")
+print(f"The Name of Second Student is: {name2} And the Age of First Student is: {Age} ")
